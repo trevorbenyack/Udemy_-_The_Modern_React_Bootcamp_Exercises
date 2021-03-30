f12948ef99150a330e52e09a52abafa7b84a0e45
Trevor Benyack <trevorbenyack@gmail.com>
2021-03-30 00:32:39 -0400

    finishes Section 04

--- 03_Props_and_More/01 - Slot Machine/Machine.js
+++ 03_Props_and_More/01 - Slot Machine/Machine.js
@@ -3,11 +3,15 @@ class Machine extends React.Component {
     
     const { s1, s2, s3 } = this.props;
     const winner = (s1 === s2) && (s2 === s3);
+    // You can store css styling in a variable and then embed it in-line
+    const colors = { fontSize: '50px', backgroundColor: 'purple'};
 
     return (
-      <div>
-        <p>{s1} {s2} {s3}</p>
-        <p>{winner ? 'Winner!' : 'Loser!'}</p>
+      <div className="Machine">
+      <label htmlFor="textInput" />
+        <p style={colors}>{s1} {s2} {s3}</p>
+        {/* You can use a ternary operator to switch between different styling */}
+        <p className={winner ? 'win' : 'lose'}>{winner ? 'Winner!' : 'Loser!'}</p>
       </div>  
     )
   }
